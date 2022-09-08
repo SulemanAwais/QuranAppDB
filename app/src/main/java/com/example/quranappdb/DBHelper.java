79c3d14c39627fa463c41ab56002a0999e1dc00f
Suleman Awais <bsef19a041@pucit.edu.pk>
2022-09-08 07:40:06 +0500

    get surah names in arabic

--- app/src/main/java/com/example/quranappdb/DBHelper.java
+++ app/src/main/java/com/example/quranappdb/DBHelper.java
@@ -1,4 +1,22 @@
 package com.example.quranappdb;
 
-public class DBHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public abstract class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context) {
+        super(context, "quran_database.db", null, 1);
+    }
+    public Cursor getArabicNames()
+    {
+        SQLiteDatabase db=this.getWritableDatabase();
+        Cursor c=db.rawQuery("SELECT SurahNameU from tsurah",null);
+        return c;
+    }
+
 }
