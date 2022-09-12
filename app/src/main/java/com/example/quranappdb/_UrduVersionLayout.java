bea4d3c05e5701f830dc871f86fa6024012b89a0
Suleman Awais <bsef19a041@pucit.edu.pk>
2022-09-13 03:14:17 +0500

    Urdu Language Translation

---
+++ app/src/main/java/com/example/quranappdb/_UrduVersionLayout.java
@@ -0,0 +1,14 @@
+package com.example.quranappdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class _UrduVersionLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_urdu_version_layout);
+    }
+}
