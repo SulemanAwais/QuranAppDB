72e02190c9186f5ec9a7a97b4387126f899476b3
Suleman Awais <bsef19a041@pucit.edu.pk>
2022-09-13 03:03:48 +0500

    Arabic and English Quran

---
+++ app/src/main/java/com/example/quranappdb/EnglishVersionLayout.java
@@ -0,0 +1,2 @@
+package com.example.quranappdb;public class EnglishVersionLayout {
+}
---
+++ app/src/main/java/com/example/quranappdb/_EnglishVersionLayout.java
@@ -0,0 +1,14 @@
+package com.example.quranappdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class _EnglishVersionLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_english_version_layout);
+    }
+}
